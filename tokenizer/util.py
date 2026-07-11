8ec203272e8c15e5d2a1fc3fb52599cb4d663746
Lewis Won <lewis.won@gmail.com>
2026-07-11 11:29:34 +0800

    working BPEgit add tokenizer/util.py !

--- tokenizer/util.py
+++ tokenizer/util.py
@@ -1,5 +1,10 @@
 import regex as re
 from collections import Counter
+from multiprocessing import Pool
+from os import cpu_count
+from functools import partial
+from typing import BinaryIO
+import os
 
 PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
 
@@ -9,6 +14,9 @@ type byte_pair = tuple[bytes, bytes]
 type encoded_word = list[bytes] # A list bytes, where each bytes is a pre-tokenized word
 type encoded_word_hashable = bytes # bytes, where each byte is a pre-tokenized word
 type PairCounts = dict[byte_pair, frequency] # A dictionary mapping a byte pair to its frequency    
+type input_path = str
+type start = int
+type end = int
 
 # Pre-tokenization
 def tokenizing_chunk(chunk):
@@ -28,33 +36,114 @@ def calculate_lexicographically_largest_byte_pair_with_highest_frequency(straigh
     max_frequency_pair = max(straight_index.items(), key=lambda item: (item[1], item[0]))
     return max_frequency_pair[0], max_frequency_pair[1]
 
+def count_pretokens(chunk_spec: tuple[input_path, start, end], special_tokens: list[str]) -> dict[encoded_word_hashable, frequency]:
+    input_path, start, end = chunk_spec
+    word_id_count : dict[encoded_word_hashable, frequency] = {}
+    escape_special_tokens = "|".join([re.escape(token) for token in special_tokens])
+    with open(input_path, "rb") as f:
+        f.seek(start)
+        chunk = f.read(end - start).decode("utf-8", errors="ignore")
+        segments = re.split(escape_special_tokens, chunk)
+        for segment in segments:
+            for match in tokenizing_chunk(segment):
+                encoded_hashable = encode_as_hashable_bytes(match.group())
+                if encoded_hashable not in word_id_count.keys():
+                    word_id_count[encoded_hashable] = 1
+                else:
+                    word_id_count[encoded_hashable] += 1
+    return word_id_count
+
+def find_chunk_boundaries(
+    file: BinaryIO,
+    desired_num_chunks: int,
+    split_special_token: bytes,
+) -> list[int]:
+    """
+    Chunk the file into parts that can be counted independently.
+    May return fewer chunks if the boundaries end up overlapping.
+    """
+    assert isinstance(split_special_token, bytes), "Must represent special token as a bytestring"
+
+    # Get total file size in bytes
+    file.seek(0, os.SEEK_END)
+    file_size = file.tell()
+    file.seek(0)
+
+    chunk_size = file_size // desired_num_chunks
+
+    # Initial guesses for chunk boundary locations, uniformly spaced
+    # Chunks start on previous index, don't include last index
+    chunk_boundaries = [i * chunk_size for i in range(desired_num_chunks + 1)]
+    chunk_boundaries[-1] = file_size
+
+    mini_chunk_size = 4096  # Read ahead by 4k bytes at a time
+
+    for bi in range(1, len(chunk_boundaries) - 1):
+        initial_position = chunk_boundaries[bi]
+        file.seek(initial_position)  # Start at boundary guess
+        while True:
+            mini_chunk = file.read(mini_chunk_size)  # Read a mini chunk
+
+            # If EOF, this boundary should be at the end of the file
+            if mini_chunk == b"":
+                chunk_boundaries[bi] = file_size
+                break
+
+            # Find the special token in the mini chunk
+            found_at = mini_chunk.find(split_special_token)
+            if found_at != -1:
+                chunk_boundaries[bi] = initial_position + found_at
+                break
+            initial_position += mini_chunk_size
+
+    # Make sure all boundaries are unique, but might be fewer than desired_num_chunks
+    return sorted(set(chunk_boundaries))
+
 
 # if __name__ == "__main__":
 def train_bpe(input_path: str, vocab_size: int = 500, special_tokens: list[str] = []) -> tuple[dict[int, bytes], list[tuple[bytes, bytes]]]:
+    num_processes = cpu_count()
+    escape_special_tokens = "|".join([re.escape(token) for token in special_tokens])
 
     ## initialization of data structures for counting byte pairs and word pairs
-    byte_pair_index : dict[byte_pair, set[encoded_word_hashable]] = {} # an inverted index telling you where each pair lives, so after a merge you only touch affected words
-    straight_index : dict[byte_pair, frequency] = {} # The thing you argmax over to pick the next merge.
     word_id_set : dict[encoded_word_hashable, encoded_word] = {} # Each unique pre-token gets an integer id.
     word_id_count : dict[encoded_word_hashable, frequency] = {} # How many times that word occured (its frequency weight)
-    
 
-    with open(input_path, "r", encoding="utf-8") as f:
-        chunk = f.read()
-    
+    with open(input_path, "rb") as f:
+        boundaries = find_chunk_boundaries(f, num_processes, b"<|endoftext|>")
+    specs = [(input_path, start, end) for start, end in zip(boundaries[:-1], boundaries[1:])]
+    with Pool(num_processes) as p:
+        count_pretokens_with_special_tokens = partial(count_pretokens, special_tokens=special_tokens)
+        for chunk_word_id_count in p.imap_unordered(count_pretokens_with_special_tokens, specs):
+            for word_id_this, frequency_this in chunk_word_id_count.items():
+                if word_id_this not in word_id_set.keys():
+                    word_id_set[word_id_this] = encode_as_bytes(word_id_this)
+                    word_id_count[word_id_this] = frequency_this
+                else:
+                    word_id_count[word_id_this] += frequency_this
 
-    ## BELOW CODE ARE PARALLELISABLE
+    # with Pool(num_processes) as p:
+    #     for chunk in p.imap_unordered(read_chunk, [(input_path, start, end) for start, end in zip(boundaries[:-1], boundaries[1:])]):
+    #         segments = re.split(escape_special_tokens, chunk)
+    #         if len(special_tokens) == 0:
+    #             segments = [chunk]
+            
+    #         for segment in segments:
+    #             ## Below code assigns a word_id to each pre-tokenized word, and counts its frequency
+    #             for match in tokenizing_chunk(segment):
+    #                 encoded_hashable = encode_as_hashable_bytes(match.group())
+    #                 encoded = encode_as_bytes(encoded_hashable)
+    #                 if encoded_hashable not in word_id_set.keys():
+    #                     word_id_set[encoded_hashable] = encoded
+    #                     word_id_count[encoded_hashable] = 1
+    #                 else:
+    #                     word_id_count[encoded_hashable] += 1
+        
 
-    ## Below code assigns a word_id to each pre-tokenized word, and counts its frequency
-    for match in tokenizing_chunk(chunk):
-        encoded_hashable = encode_as_hashable_bytes(match.group())
-        encoded = encode_as_bytes(encoded_hashable)
-        if encoded_hashable not in word_id_set.keys():
-            word_id_set[encoded_hashable] = encoded
-            word_id_count[encoded_hashable] = 1
-        else:
-            word_id_count[encoded_hashable] += 1
-    
+    ### BELOW CODE ARE NON-PARALLELISABLE  
+    byte_pair_index : dict[byte_pair, set[encoded_word_hashable]] = {} # an inverted index telling you where each pair lives, so after a merge you only touch affected words
+    straight_index : dict[byte_pair, frequency] = {} # The thing you argmax over to pick the next merge.
+
     ## Below code builds the byte pair index, straight index, and reverse index for the pre-tokenized words
     for word_id_this in word_id_set.keys():
         word_id_this_frequency = word_id_count[word_id_this]
@@ -63,8 +152,6 @@ def train_bpe(input_path: str, vocab_size: int = 500, special_tokens: list[str] 
         for tuple_pair, frequency_count in counters.items():
             straight_index[tuple_pair] = straight_index.get(tuple_pair, 0) + frequency_count * word_id_this_frequency
             byte_pair_index.setdefault(tuple_pair, set()).add(word_id_this)
-
-    ### BELOW CODE ARE NON-PARALLELISABLE  
 
     vocab : dict[int, bytes] = {i: bytes([i]) for i in range(256)} # The vocab is a mapping from the integer id to the bytes representation of the pre-tokenized word
     for i in range(256, 256 + len(special_tokens)):
